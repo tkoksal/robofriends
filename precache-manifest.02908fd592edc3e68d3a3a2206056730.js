28f28a64411e95d7475f45b4be93b1d1ca7b2e8f
tkoksal <tamerkoksal@hotmail.com>
2018-12-05 16:29:15 +0300

    Updates

---
+++ precache-manifest.02908fd592edc3e68d3a3a2206056730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "c37b340cdf7cf8fc8f8c",
+    "url": "/robofriends/static/js/main.c37b340c.chunk.js"
+  },
+  {
+    "revision": "d73f99ced0022c0a54b4",
+    "url": "/robofriends/static/js/1.d73f99ce.chunk.js"
+  },
+  {
+    "revision": "c37b340cdf7cf8fc8f8c",
+    "url": "/robofriends/static/css/main.f4c3efbe.chunk.css"
+  },
+  {
+    "revision": "d73f99ced0022c0a54b4",
+    "url": "/robofriends/static/css/1.3a6c3064.chunk.css"
+  },
+  {
+    "revision": "4876c011df3ea9401aae931e87c0a301",
+    "url": "/robofriends/index.html"
+  }
+];
